2500e1093c5152030dad9a3ca70db8a40af71d30
Erlow <erlowgaming@gmail.com>
2026-04-04 11:05:32 +0200

    Update @orbisite/blocks dependency to version 0.2.6 in package.json and package-lock.json, and adjust deployment configuration in GitHub Actions to reflect the new version. Additionally, add spaLinkComponent to page configuration for enhanced functionality.

--- src/data/page.config.js
+++ src/data/page.config.js
@@ -74,6 +74,7 @@ export function buildPageConfig(content, locale = 'fr', onLocaleChange = () => {
         color: 'primary',
         imageUrl: images.hero,
         imageAlt: hero.imageAlt,
+        spaLinkComponent,
       },
     }),
     features: () => ({
